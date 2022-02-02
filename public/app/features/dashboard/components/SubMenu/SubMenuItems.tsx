daacc64df6635178947af5a0575246409d445b70
Gautam Punhani <gautamp@thoughtworks.com>
2022-02-02 08:19:09 +0530

    Removed few of the dom elements in case of kiosk TV and displayed the filters in kiosk tv mode

--- public/app/features/dashboard/components/SubMenu/SubMenuItems.tsx
+++ public/app/features/dashboard/components/SubMenu/SubMenuItems.tsx
@@ -1,7 +1,11 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
-import { VariableHide, VariableModel } from '../../../variables/types';
+import { TextBoxVariableModel, VariableHide, VariableModel } from '../../../variables/types';
 import { selectors } from '@grafana/e2e-selectors';
 import { PickerRenderer } from '../../../variables/pickers/PickerRenderer';
+import { ALL_VARIABLE_TEXT } from '../../../variables/state/types';
+import { getLocationSrv } from '@grafana/runtime';
+import { getTemplateSrv } from '../../../templating/template_srv';
+import { Button } from '@grafana/ui';
 
 interface Props {
   variables: VariableModel[];
@@ -13,6 +17,32 @@ export const SubMenuItems: FunctionComponent<Props> = ({ variables }) => {
   useEffect(() => {
     setVisibleVariables(variables.filter((state) => state.hide !== VariableHide.hideVariable));
   }, [variables]);
+
+  function onClearAllFilters(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    const updateQuery: any = {};
+    const templateSrv = getTemplateSrv();
+
+    visibleVariables.map((variable) => {
+      const variableName = `var-${variable.id}`;
+      let allValue = templateSrv.getAllValue(variable);
+      if (allValue === ALL_VARIABLE_TEXT) {
+        updateQuery[variableName] = allValue;
+      } else {
+        let variableAsText = variable as TextBoxVariableModel;
+        if (variableAsText) {
+          updateQuery[variableName] = variableAsText.originalQuery;
+        }
+      }
+    });
+
+    getLocationSrv().update({
+      query: updateQuery,
+      partial: true,
+      replace: true,
+    });
+  }
 
   if (visibleVariables.length === 0) {
     return null;
@@ -31,6 +61,10 @@ export const SubMenuItems: FunctionComponent<Props> = ({ variables }) => {
           </div>
         );
       })}
+
+      <Button onClick={onClearAllFilters} fill={'text'}>
+        Clear All
+      </Button>
     </>
   );
 };
