01dda133fe0bf9258c519aef41ef5f5c63fa9140
pankaj <pankaj29606@gmail.com>
2022-07-01 16:19:06 +0530

    fix: centering spinner at centre

--- public/app/features/dashboard/components/DashboardLoading/DashboardLoading.tsx
+++ public/app/features/dashboard/components/DashboardLoading/DashboardLoading.tsx
@@ -47,7 +47,7 @@ export const getStyles = (theme: GrafanaTheme2) => {
 
   return {
     dashboardLoading: css`
-      height: 60vh;
+      height: 100vh;
       display: flex;
       opacity: 0%;
       align-items: center;
