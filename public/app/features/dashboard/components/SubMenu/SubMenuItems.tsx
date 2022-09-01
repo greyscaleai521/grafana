d09e3c44e90321659ab27a1cb41f8f535dde3e4a
Chandrakanth <chandrakanth.ande@greyscaleai.com>
2022-09-01 17:05:07 +0530

    Removed few of the dom elements in case of kiosk TV and displayed the filters in kiosk tv mode

--- public/app/features/dashboard/components/SubMenu/SubMenuItems.tsx
+++ public/app/features/dashboard/components/SubMenu/SubMenuItems.tsx
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 import { TypedVariableModel, VariableHide } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
+import { getLocationSrv } from '@grafana/runtime';
+import { Button } from '@grafana/ui';
 
+import { getTemplateSrv } from '../../../templating/template_srv';
+import { ALL_VARIABLE_TEXT } from '../../../variables/constants';
 import { PickerRenderer } from '../../../variables/pickers/PickerRenderer';
+import { TextBoxVariableModel, VariableHide, VariableModel } from '../../../variables/types';
 
 interface Props {
   variables: TypedVariableModel[];
@@ -16,6 +21,32 @@ export const SubMenuItems = ({ variables, readOnly }: Props) => {
   useEffect(() => {
     setVisibleVariables(variables.filter((state) => state.hide !== VariableHide.hideVariable));
   }, [variables]);
+
+  function onClearAllFilters(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    const updateQuery: any = {};
+    const templateSrv = getTemplateSrv();
+
+    visibleVariables.map((variable) => {
+      const variableName = `var-${variable.id}`;
+      let allValue = templateSrv.getAllValue(variable);
+      if (allValue === ALL_VARIABLE_TEXT) {
+        updateQuery[variableName] = allValue;
+      } else {
+        let variableAsText = variable as TextBoxVariableModel;
+        if (variableAsText) {
+          updateQuery[variableName] = variableAsText.originalQuery;
+        }
+      }
+    });
+
+    getLocationSrv().update({
+      query: updateQuery,
+      partial: true,
+      replace: true,
+    });
+  }
 
   if (visibleVariables.length === 0) {
     return null;
@@ -32,6 +63,10 @@ export const SubMenuItems = ({ variables, readOnly }: Props) => {
           <PickerRenderer variable={variable} readOnly={readOnly} />
         </div>
       ))}
+
+      <Button onClick={onClearAllFilters} fill={'text'}>
+        Clear All
+      </Button>
     </>
   );
 };
