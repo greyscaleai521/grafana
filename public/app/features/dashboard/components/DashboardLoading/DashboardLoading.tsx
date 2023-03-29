91f583ad953727e9100d473df05c95ef6386f68d
Chandrakanth <chandrakanth.ande@greyscaleai.com>
2023-03-29 16:43:22 +0530

    feat: loading spinner

--- public/app/features/dashboard/components/DashboardLoading/DashboardLoading.tsx
+++ public/app/features/dashboard/components/DashboardLoading/DashboardLoading.tsx
@@ -2,32 +2,34 @@ import { css, keyframes } from '@emotion/css';
 import React from 'react';
 
 import { GrafanaTheme2 } from '@grafana/data';
-import { locationService } from '@grafana/runtime';
-import { Button, HorizontalGroup, Spinner, useStyles2, VerticalGroup } from '@grafana/ui';
+// import { locationService } from '@grafana/runtime';
+import { HorizontalGroup, useStyles2, VerticalGroup } from '@grafana/ui';
 import { DashboardInitPhase } from 'app/types';
 
+import { LoadingSpinner } from 'app/core/components/Loaders/LoadingSpinner';
 export interface Props {
   initPhase: DashboardInitPhase;
 }
 
 export const DashboardLoading = ({ initPhase }: Props) => {
   const styles = useStyles2(getStyles);
-  const cancelVariables = () => {
-    locationService.push('/');
-  };
+  // const cancelVariables = () => {
+  //   locationService.push('/');
+  // };
 
   return (
     <div className={styles.dashboardLoading}>
       <div className={styles.dashboardLoadingText}>
         <VerticalGroup spacing="md">
           <HorizontalGroup align="center" justify="center" spacing="xs">
-            <Spinner inline={true} /> {initPhase}
+            {/* <Spinner inline={true} /> {initPhase} */}
+            <LoadingSpinner />
           </HorizontalGroup>{' '}
-          <HorizontalGroup align="center" justify="center">
+          {/* <HorizontalGroup align="center" justify="center">
             <Button variant="secondary" size="md" icon="repeat" onClick={cancelVariables}>
               Cancel loading dashboard
             </Button>
-          </HorizontalGroup>
+          </HorizontalGroup> */}
         </VerticalGroup>
       </div>
     </div>
