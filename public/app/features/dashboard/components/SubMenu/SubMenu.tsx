f3e051a9e6e0d6acb6967f0f1b194c1972a80d72
Chandrakanth <chandrakanth.ande@greyscaleai.com>
2024-04-25 17:31:42 +0530

    Add category and grouping Filters Feature

--- public/app/features/dashboard/components/SubMenu/SubMenu.tsx
+++ public/app/features/dashboard/components/SubMenu/SubMenu.tsx
@@ -2,7 +2,14 @@ import { css } from '@emotion/css';
 import React, { PureComponent } from 'react';
 import { connect, MapStateToProps } from 'react-redux';
 
-import { AnnotationQuery, DataQuery, TypedVariableModel, GrafanaTheme2 } from '@grafana/data';
+import {
+  AnnotationQuery,
+  DataQuery,
+  TypedVariableModel,
+  GrafanaTheme2,
+  VariableHide,
+  VariableWithOptions,
+} from '@grafana/data';
 import { DashboardLink } from '@grafana/schema';
 import { stylesFactory, Themeable2, withTheme2 } from '@grafana/ui';
 
@@ -11,6 +18,7 @@ import { getSubMenuVariables, getVariablesState } from '../../../variables/state
 import { DashboardModel } from '../../state';
 
 import { Annotations } from './Annotations';
+import { CategoryBar } from './CategoryBar';
 import { DashboardLinks } from './DashboardLinks';
 import { SubMenuItems } from './SubMenuItems';
 
@@ -28,7 +36,26 @@ interface DispatchProps {}
 
 type Props = OwnProps & ConnectedProps & DispatchProps;
 
-class SubMenuUnConnected extends PureComponent<Props> {
+function isDefault(filter: VariableWithOptions) {
+  return filter?.current?.value === undefined ||
+    filter?.current?.value?.toString() === '' ||
+    filter?.current?.value?.toString() === 'All' ||
+    filter?.current?.value?.toString() === '$__all' ||
+    filter?.current?.value?.toString() === 'Production' ||
+    filter?.current?.value?.toString() === 'Max Resolution'
+    ? true
+    : false;
+}
+
+class SubMenuUnConnected extends PureComponent<Props, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      selectedCategory: 0,
+      uniqueCategories: [],
+      categoryFilterCounter: {},
+    };
+  }
   onAnnotationStateChanged = (updatedAnnotation: AnnotationQuery<DataQuery>) => {
     // we're mutating dashboard state directly here until annotations are in Redux.
     for (let index = 0; index < this.props.dashboard.annotations.list.length; index++) {
@@ -46,6 +73,54 @@ class SubMenuUnConnected extends PureComponent<Props> {
     e.preventDefault();
   };
 
+  onCategoryChange = (index: number) => {
+    this.setState({
+      selectedCategory: index,
+    });
+  };
+
+  onFilterCounterChange = (counter: Record<string, number>) => {
+    this.setState({
+      categoryFilterCounter: counter,
+    });
+  };
+
+  componentDidUpdate(prevProps: Props, prevState: any) {
+    const { variables } = this.props;
+    let counter: Record<string, number> = {};
+
+    variables.forEach((variable) => {
+      if (variable.category !== undefined && variable.category !== '' && variable.category !== null) {
+        if (variable.hide !== VariableHide.hideVariable && !isDefault(variable as VariableWithOptions)) {
+          if (Object.keys(counter).includes(variable.category)) {
+            counter[variable.category] += 1;
+          } else {
+            counter[variable.category] = 1;
+          }
+        }
+      }
+    });
+    if (JSON.stringify(counter) !== JSON.stringify(prevState.categoryFilterCounter)) {
+      this.setState({
+        categoryFilterCounter: counter,
+      });
+    }
+  }
+
+  componentDidMount() {
+    const { variables } = this.props;
+    const uniqueCategories = new Set();
+    variables.forEach((variable) => {
+      if (variable.category !== undefined && variable.category !== '' && variable.category !== null) {
+        uniqueCategories.add(variable.category);
+      }
+    });
+
+    this.setState({
+      uniqueCategories: Array.from(uniqueCategories),
+    });
+  }
+
   render() {
     const { dashboard, variables, links, annotations, theme } = this.props;
 
@@ -58,18 +133,35 @@ class SubMenuUnConnected extends PureComponent<Props> {
     const readOnlyVariables = dashboard.meta.isSnapshot ?? false;
 
     return (
-      <div className={styles.submenu}>
-        <form aria-label="Template variables" className={styles.formStyles} onSubmit={this.disableSubmitOnEnter}>
-          <SubMenuItems variables={variables} readOnly={readOnlyVariables} />
-        </form>
-        <Annotations
-          annotations={annotations}
-          onAnnotationChanged={this.onAnnotationStateChanged}
-          events={dashboard.events}
-        />
-        <div className={styles.spacer} />
-        {dashboard && <DashboardLinks dashboard={dashboard} links={links} />}
-      </div>
+      <>
+        <div className="submenu-controls-top">
+          <CategoryBar
+            categories={this.state.uniqueCategories}
+            onCategoryChange={this.onCategoryChange}
+            selecedCategory={this.state.selectedCategory}
+            categoryFilterCounter={this.state.categoryFilterCounter}
+            variables={variables}
+          />
+        </div>
+        <div className={styles.submenu}>
+          <form aria-label="Template variables" className={styles.formStyles} onSubmit={this.disableSubmitOnEnter}>
+            <SubMenuItems
+              variables={variables}
+              readOnly={readOnlyVariables}
+              selectedCategory={this.state.selectedCategory}
+              categories={Array.from(this.state.uniqueCategories)}
+            />
+          </form>
+          <Annotations
+            annotations={annotations}
+            onAnnotationChanged={this.onAnnotationStateChanged}
+            events={dashboard.events}
+          />
+          <div className={styles.spacer} />
+          {dashboard && <DashboardLinks dashboard={dashboard} links={links} />}
+          <div className="clearfix" />
+        </div>
+      </>
     );
   }
 }
@@ -96,7 +188,7 @@ const getStyles = stylesFactory((theme: GrafanaTheme2) => {
       align-content: flex-start;
       align-items: flex-start;
       gap: ${theme.spacing(1)} ${theme.spacing(2)};
-      padding: 0 0 ${theme.spacing(1)} 0;
+      padding: ${theme.spacing(1)} 0 ${theme.spacing(1)} ${theme.spacing(1)};
     `,
     spacer: css({
       flexGrow: 1,
--- public/app/features/dashboard/components/SubMenu/SubMenuItems.tsx
+++ public/app/features/dashboard/components/SubMenu/SubMenuItems.tsx
@@ -1,21 +1,70 @@
 import React, { useEffect, useState } from 'react';
 
-import { TypedVariableModel, VariableHide } from '@grafana/data';
+import { TypedVariableModel, VariableHide, TextBoxVariableModel } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
+import { getLocationSrv } from '@grafana/runtime';
+import { Button } from '@grafana/ui';
 
+import { getTemplateSrv } from '../../../templating/template_srv';
+import { ALL_VARIABLE_TEXT } from '../../../variables/constants';
 import { PickerRenderer } from '../../../variables/pickers/PickerRenderer';
 
 interface Props {
   variables: TypedVariableModel[];
   readOnly?: boolean;
+  selectedCategory?: number;
+  categories?: any;
 }
 
-export const SubMenuItems = ({ variables, readOnly }: Props) => {
+export const SubMenuItems = ({ variables, readOnly, selectedCategory, categories = [] }: Props) => {
+  const optionVariables = variables as TypedVariableModel[];
   const [visibleVariables, setVisibleVariables] = useState<TypedVariableModel[]>([]);
 
   useEffect(() => {
-    setVisibleVariables(variables.filter((state) => state.hide !== VariableHide.hideVariable));
-  }, [variables]);
+    let visibleVariables = [];
+    if (categories.length && selectedCategory !== undefined) {
+      visibleVariables = optionVariables.filter(
+        (state) => state.hide !== VariableHide.hideVariable && state.category === categories[selectedCategory]
+      );
+    } else {
+      visibleVariables = optionVariables.filter((state) => state.hide !== VariableHide.hideVariable);
+    }
+    setVisibleVariables(visibleVariables);
+  }, [optionVariables, selectedCategory, categories]);
+
+  function onClearCategoryFilters(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    const updateQuery: any = {};
+    const templateSrv = getTemplateSrv();
+
+    optionVariables
+      .filter((variable) => {
+        if (categories.length && selectedCategory !== undefined) {
+          return variable.hide !== VariableHide.hideVariable && variable.category === categories[selectedCategory];
+        } else {
+          return variable.hide !== VariableHide.hideVariable;
+        }
+      })
+      .map((variable) => {
+        const variableName = `var-${variable.id}`;
+        let allValue = templateSrv.getAllValue(variable);
+        if (allValue === ALL_VARIABLE_TEXT) {
+          updateQuery[variableName] = allValue;
+        } else {
+          let variableAsText = variable as TextBoxVariableModel;
+          if (variableAsText) {
+            updateQuery[variableName] = variableAsText.originalQuery;
+          }
+        }
+      });
+
+    getLocationSrv().update({
+      query: updateQuery,
+      partial: true,
+      replace: true,
+    });
+  }
 
   if (visibleVariables.length === 0) {
     return null;
@@ -32,6 +81,11 @@ export const SubMenuItems = ({ variables, readOnly }: Props) => {
           <PickerRenderer variable={variable} readOnly={readOnly} />
         </div>
       ))}
+      <Button className="clearall-btn" onClick={onClearCategoryFilters} fill={'text'}>
+        Clear
+      </Button>
     </>
   );
 };
+
+SubMenuItems.displayName = 'SubMenu';
