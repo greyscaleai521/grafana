6cb0f84bcc69fb1d51758ff360ac4bbf52f5273d
gsai-pooja <pooja@greyscaleai.com>
2026-06-12 12:12:49 +0530

    updated range macro

--- pkg/tsdb/grafana-postgresql-datasource/macros.go
+++ pkg/tsdb/grafana-postgresql-datasource/macros.go
@@ -1,16 +1,22 @@
 package postgres
 
 import (
+	"encoding/csv"
 	"fmt"
+	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/gtime"
 
+	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/tsdb/grafana-postgresql-datasource/sqleng"
 )
+
+var macrosLogger = log.New("macros")
 
 const rsIdentifier = `([_a-zA-Z0-9]+)`
 const sExpr = `\$` + rsIdentifier + `\(([^\)]*)\)`
@@ -153,21 +159,27 @@ func (m *postgresMacroEngine) Interpolate(query *backend.DataQuery, timeRange ba
 		// if there is a ',' directly after the macro call $__timeGroup is probably used
 		// in the old way. Inside window function ORDER BY $__timeGroup will be followed
 		// by ')'
-		if groups[1] == "__timeGroup" {
+		macroName := groups[1]
+		if macroName == "__timeGroup" {
 			if index := strings.Index(sql, groups[0]); index >= 0 {
 				index += len(groups[0])
 				// check for character after macro expression
 				if len(sql) > index && sql[index] == ',' {
-					groups[1] = "__timeGroupAlias"
+					macroName = "__timeGroupAlias"
 				}
 			}
 		}
 
-		args := strings.Split(groups[2], ",")
-		for i, arg := range args {
-			args[i] = strings.Trim(arg, " ")
+		params := groups[2]
+		args, err := m.extractArgsFromParams(params)
+		if err != nil && macroError == nil {
+			macroError = err
+			macrosLogger.Debug("macroError", "args", fmt.Sprintf("%v", macroError))
+			macrosLogger.Debug("groups[2]", "args", fmt.Sprintf("%v", params))
+			return "macro_error()"
 		}
-		res, err := m.evaluateMacro(timeRange, query, groups[1], args)
+
+		res, err := m.evaluateMacro(timeRange, query, macroName, args)
 		if err != nil && macroError == nil {
 			macroError = err
 			return "macro_error()"
@@ -182,8 +194,27 @@ func (m *postgresMacroEngine) Interpolate(query *backend.DataQuery, timeRange ba
 	return sql, nil
 }
 
+func (m *postgresMacroEngine) extractArgsFromParams(params string) ([]string, error) {
+	params = strings.ReplaceAll(strings.TrimSpace(params), "\n", "")
+	if params == "" {
+		return []string{""}, nil
+	}
+	r := csv.NewReader(strings.NewReader(params))
+	r.LazyQuotes = true
+	r.TrimLeadingSpace = true
+	args, err := r.Read()
+	if err != nil {
+		return nil, err
+	}
+	for i, arg := range args {
+		args[i] = strings.TrimSpace(arg)
+	}
+	return args, nil
+}
+
 //nolint:gocyclo
 func (m *postgresMacroEngine) evaluateMacro(timeRange backend.TimeRange, query *backend.DataQuery, name string, args []string) (string, error) {
+	macrosLogger.Debug("evaluating macros", "args", fmt.Sprintf("%v", args))
 	switch name {
 	case "__time":
 		if len(args) == 0 {
@@ -276,7 +307,135 @@ func (m *postgresMacroEngine) evaluateMacro(timeRange backend.TimeRange, query *
 			return tg + " AS \"time\"", nil
 		}
 		return "", err
+	// GSAI - custom macros
+	case "__isTimeFromOutsideThreshold":
+		if len(args) != 1 {
+			return "", fmt.Errorf("expecting threshold argument of type int alone to be passed: passed arguments are %v", args)
+		}
+		var thresholdInDays int
+		_, err := fmt.Sscan(args[0], &thresholdInDays)
+		if err != nil {
+			return "", fmt.Errorf("unable to parse threshold '%s': %w", args[0], err)
+		}
+		durationInHours := time.Now().UTC().Sub(timeRange.From.UTC()).Hours()
+		if int(durationInHours) > (24 * thresholdInDays) {
+			return "true", nil
+		}
+		return "false", nil
+	case "__isNull":
+		for _, arg := range args {
+			if arg != "NULL" {
+				return "false", nil
+			}
+		}
+		return "true", nil
+	case "__constructPredicates":
+		if len(args) == 1 && args[0] == "" {
+			return "true", nil
+		}
+		var excludedValues []string
+		type Pair struct {
+			Key   string
+			Query string
+		}
+		var macroArguments []Pair
+		for _, arg := range args {
+			argList := strings.Split(arg, ":")
+			macrosLogger.Debug("splitting args", "argList", fmt.Sprintf("%v", argList))
+			if len(argList) != 2 {
+				return "", fmt.Errorf("error in parsing argument: %s not in key value pair format", arg)
+			}
+			keyName := strings.TrimSpace(argList[0])
+			valuesString := strings.TrimSpace(argList[1])
+			if keyName == "exclude_values" {
+				excludedValues = strings.Split(valuesString, ",")
+			} else {
+				macroArguments = append(macroArguments, Pair{keyName, valuesString})
+			}
+		}
+		var filtersList []string
+		for _, macroArgument := range macroArguments {
+			values, err := url.ParseQuery(macroArgument.Query)
+			if err != nil {
+				return "", fmt.Errorf("error while parsing query params: %w", err)
+			}
+			for _, value := range values {
+				includeValues := m.removeFromSlice(value, excludedValues)
+				if len(includeValues) != 0 {
+					formattedValues := m.escapeSqlSingleQuotes(includeValues)
+					formattedArgList := fmt.Sprintf("%s in (%s)", macroArgument.Key, formattedValues)
+					filtersList = append(filtersList, formattedArgList)
+				}
+			}
+		}
+		if len(filtersList) == 0 {
+			return "true", nil
+		}
+		return strings.Join(filtersList, " and "), nil
+	case "__constructRangePredicate":
+		if len(args) != 3 {
+			return "", fmt.Errorf("expecting field name, value and conversion factor to be passed: passed arguments are %v", args)
+		}
+		field := strings.TrimSpace(args[0])
+		value := strings.TrimSpace(args[1])
+		convFactor := strings.TrimSpace(args[2])
+
+		if value == "" {
+			return "true", nil
+		}
+
+		values := strings.Split(value, "-")
+		var valuesCasted []float64
+
+		convFactorCasted, err := strconv.ParseFloat(strings.TrimSpace(convFactor), 64)
+		if err != nil {
+			return "", err
+		}
+
+		for _, item := range values {
+			if floatValue, err := strconv.ParseFloat(strings.TrimSpace(item), 64); err == nil {
+				valuesCasted = append(valuesCasted, floatValue)
+			} else {
+				return "", err
+			}
+		}
+
+		if len(valuesCasted) == 2 {
+			return fmt.Sprintf("%v between %v and %v", field, valuesCasted[0]*convFactorCasted, valuesCasted[1]*convFactorCasted), nil
+		} else if len(valuesCasted) == 1 {
+			return fmt.Sprintf("%v = %v", field, valuesCasted[0]*convFactorCasted), nil
+		}
+		return "", fmt.Errorf("expecting either float value or range: passed argument are %v", args[1])
 	default:
 		return "", fmt.Errorf("unknown macro %q", name)
 	}
+}
+
+func (m *postgresMacroEngine) containsInSlice(value string, slice []string) bool {
+	for _, item := range slice {
+		if item == value {
+			return true
+		}
+	}
+	return false
+}
+
+func (m *postgresMacroEngine) removeFromSlice(values []string, slice []string) []string {
+	var result []string
+	for _, item := range values {
+		if !m.containsInSlice(item, slice) {
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+func (m *postgresMacroEngine) escapeSqlSingleQuotes(values []string) string {
+	for i := range values {
+		values[i] = strings.TrimSpace(values[i])
+		values[i] = strings.Trim(values[i], `'`)
+		values[i] = strings.ReplaceAll(values[i], `'`, `''`)
+		values[i] = fmt.Sprintf(`'%s'`, values[i])
+	}
+	return strings.Join(values, ",")
 }
