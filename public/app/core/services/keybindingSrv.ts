9bee4374760a91e250ba76cd5d581d56ad01cbbd
gsai-pooja <pooja@greyscaleai.com>
2026-06-12 14:30:34 +0530

    Update color variables in Sass files and theme.ts

--- public/app/core/services/keybindingSrv.ts
+++ public/app/core/services/keybindingSrv.ts
@@ -173,7 +173,10 @@ export class KeybindingSrv {
 
     const { kioskMode } = this.chromeService.state.getValue();
     if (kioskMode) {
-      this.chromeService.exitKioskMode();
+      const removeKioskOnEscape = search.removeKioskOnEscape === false;
+      if (!removeKioskOnEscape) {
+        this.chromeService.exitKioskMode();
+      }
     }
   }
 
